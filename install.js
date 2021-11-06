47d8658ac40df51ebda5a3aba4b89f7bbf0d4448
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-11-06 15:33:48 -0400

    Add CORS support via proxy

--- install.js
+++ install.js
@@ -1,5 +1,5 @@
 async function get(url) {
-  const response = await fetch("https://htmltojson.pythonanywhere.com/get/?link=" + url);
+  const response = await fetch("https://proxy-for-cors.herokuapp.com/https://python-html-json.herokuapp.com/get/?link=" + url);
   const data = await response.json();
   return data.result;
 }
